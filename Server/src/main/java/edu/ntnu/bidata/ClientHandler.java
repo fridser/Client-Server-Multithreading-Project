c93447cff4616a66c5fd139b0603e8475175ba2b
Martin BF <martinbf@hotmail.no>
2026-02-09 13:54:06 +0100

    Made clientHandelr for multiThreadServer

--- Server/src/main/java/edu/ntnu/bidata/ClientHandler.java
+++ Server/src/main/java/edu/ntnu/bidata/ClientHandler.java
@@ -1,13 +1,29 @@
 package edu.ntnu.bidata;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
-  private Socket client;
+  private final Socket client;
+  private final CalculatorLogic logic;
 
   public ClientHandler(Socket client) {
     this.client = client;
+    this.logic = new CalculatorLogic();
   }
 
-
+  @Override
+  public void run() {
+    try {
+      byte[] input = client.getInputStream().readAllBytes();
+      String message = new String(input);
+      String output = String.valueOf(this.logic.handleCommand(message));
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      out.write(output.getBytes());
+      client.close();
+    } catch (IOException e) {
+      throw new RuntimeException("Not Implemented");
+    }
+  }
 }
