d49efe1acef57e2c84be94ac01587f6afe0b2b61
Martin BF <martinbf@hotmail.no>
2026-02-09 13:16:34 +0100

    Martin merge

--- Server/src/main/java/edu/ntnu/bidata/MultiThreadServer.java
+++ Server/src/main/java/edu/ntnu/bidata/MultiThreadServer.java
@@ -2,8 +2,20 @@ package edu.ntnu.bidata;
 
 public class MultiThreadServer {
   private ServerSocket serverSocket;
+  private volatile boolean running = false;
 
   public MultiThreadServer(InetAddress address, int port) throws IOException {
     serverSocket = new ServerSocket(address,port);
+  }
+
+
+  public void start() {
+    running = true;
+    while (running) {
+      try {
+        Socket client = serverSocket.accept();
+        Thread thread = new Thread();
+      }
+    }
   }
 }
