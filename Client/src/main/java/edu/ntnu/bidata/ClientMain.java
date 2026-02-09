12346082a0b1740ac8b46ba365266f17a87763d4
Vebjørn Otneim Myklebust <vebjomyk@stud.ntnu.no>
2026-02-09 13:47:04 +0100

    Created ClientMain class.

---
+++ Client/src/main/java/edu/ntnu/bidata/ClientMain.java
@@ -0,0 +1,11 @@
+package edu.ntnu.bidata;
+
+/**
+ * Entry point for the Client application. This class can be used to start the client application.
+ * A class like this is necessary to start a JavaFX application, since the main method must be in a separate class from the Application class.
+ */
+public class ClientMain {
+  public static void main(String[] args) {
+    ClientApp.appMain(args);
+  }
+}
