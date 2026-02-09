48ecbbb720daa40a1a94a57b33778f7d3916b95f
Vebjørn Otneim Myklebust <vebjomyk@stud.ntnu.no>
2026-02-09 13:48:26 +0100

    Created ClientApp class.

---
+++ Client/src/main/java/edu/ntnu/bidata/ClientApp.java
@@ -0,0 +1,21 @@
+package edu.ntnu.bidata;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientApp extends Application {
+  @Override
+  public void start(Stage stage) throws Exception {
+
+  }
+
+  /**
+   * Launch the JavaFX application. This method is called from the main method in ClientMain.
+   * The main method is used to start the application, and this method is used to launch the JavaFX application.
+   *
+   * @param args the command line arguments passed to the application.
+   */
+  public static void appMain(String[] args) {
+    launch(args);
+  }
+}
