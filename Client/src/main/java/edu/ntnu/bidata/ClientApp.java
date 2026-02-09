fd375f15f40d398e9f2a33e30cef3e0d46b94f10
Vebjørn Otneim Myklebust <vebjomyk@stud.ntnu.no>
2026-02-09 14:03:22 +0100

    Created basic GUI components.

--- Client/src/main/java/edu/ntnu/bidata/ClientApp.java
+++ Client/src/main/java/edu/ntnu/bidata/ClientApp.java
@@ -1,12 +1,46 @@
 package edu.ntnu.bidata;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * Responsible for the graphical user interface of the client application. The view contains buttons
+ * which control the calculator.
+ */
 public class ClientApp extends Application {
   @Override
   public void start(Stage stage) throws Exception {
+    BorderPane rootNode = new BorderPane();
+    // VBox for separating the sections of the calculator.
+    VBox vbox = new VBox();
+    rootNode.setCenter(vbox);
 
+    // Add the number buttons grid to the vbox.
+    vbox.getChildren().add(createNumberButtons());
+
+    Scene scene = new Scene(rootNode, 320, 600);
+    stage.setScene(scene);
+    stage.setTitle("Calculator Client");
+    stage.show();
+  }
+
+  /**
+   * Create a grid of numbered buttons for the calculator.
+   *
+   * @return a GridPane containing the buttons for the calculator.
+   */
+  public GridPane createNumberButtons() {
+    GridPane grid = new GridPane();
+    for (int i = 1; i <= 9; i++) {
+      Button button = new Button(Integer.toString(i));
+      grid.add(button, (i - 1) % 3, (i - 1) / 3);
+    }
+    return grid;
   }
 
   /**
